14f4c8b055d00e9486dc1b78dc104a668157eaeb
BhavyaGupta771 <guptabhavya044@gmail.com>
2025-06-23 05:46:36 +0000

    bhav

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -8,6 +8,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Starting Spring Boot Application...");
+		System.out.println("Started successfully! ");
 		SpringApplication.run(Main.class, args);
 	}
 
